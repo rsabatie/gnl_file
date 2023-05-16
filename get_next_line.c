23c7d75c050059e3058e0d7008aa8051f2a75920
Rafael <rafael@MacBook-Pro-de-Admin.local>
2023-05-16 09:53:01 +0200

    .

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,19 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 19:46:09 by rafael            #+#    #+#             */
-/*   Updated: 2023/05/14 20:41:08 by rafael           ###   ########.fr       */
+/*   Updated: 2023/05/15 22:37:42 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void    ft_read_file(char **txt_save, fd)
+void	ft_read_file(char **txt_save, fd)
 {
 	int	i;
 	char buffer[BUFFER_SIZE + 1];
 
+	if (!txt_save)
+		*
 	i = 1;
 	if (i < 0 && !*txt_save)
 	{
@@ -25,10 +27,45 @@ void    ft_read_file(char **txt_save, fd)
 	}
 	while (i > 0)
 	{
-	ft_bzero(buffer, BUFFER_SIZE);
+	ft_bzero(buffer, BUFFER_SIZE + 1);
 	i = read(fd, buffer, BUFFER_SIZE);
 	*txt_save = ft_strjoin(*txt_save, buffer);
 	}
+}
+
+char	*ft_make_line(char **txt_save)
+{
+	int	i;
+	char *line;
+
+	i = 0;
+	while (txt_save[i] && *txt_save[i] != '\n')
+		i++;
+	if (*txt_save[i] == '\n')
+		i++;
+	line = ft_calloc(i + 1, sizeof(char));
+	if (!line)
+		return (free(txt_save), NULL);
+	i = 0;
+	while (txt_save[i])
+	{
+		if (*txt_save[i++] == '\n')
+		{
+			line[i] = *txt_save[i]
+			return (line);
+		}
+		line[i] = *txt_save[i];
+		i++;
+	}
+	return (line);
+}
+
+void	ft_build_next(char **txt_save)
+{
+	char *tmp;
+	
+	tmp = ft_strdup(*txt_save);
+	
 }
 
 int main(void)
